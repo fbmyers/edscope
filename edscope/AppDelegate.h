0a2cadefc930e2d6cfd1197ade1ca14140c850d4
fbmyers <frankiemyers@Arunans-MacBook-Pro.local>
2013-11-18 16:11:19 -0800

    Initial Commit

---
+++ edscope/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  edscope
+//
+//  Created by Frankie Myers on 11/18/13.
+//  Copyright (c) 2013 UC Berkeley Fletcher Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ edscope/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  edscope
+//
+//  Created by Frankie Myers on 11/18/13.
+//  Copyright (c) 2013 UC Berkeley Fletcher Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
